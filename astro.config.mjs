8b08fb17a81ded132331f293504e65fc6829392a
Jürgen Leschner <jurgen@jldec.me>
2026-03-29 00:28:29 -0500

    fix canonical, add meta images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   site: 'https://jldec.me',
   trailingSlash: 'never',
+  compressHTML: false,
   build: {
     format: 'file'
   },
